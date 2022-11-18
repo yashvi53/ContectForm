210106cfa5c1203e57cc2da9f322e9ec6042abcc
unknown <yashvisoni.vasyerp.com>
2022-11-18 18:03:24 +0530

    second comit

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,184 +1,281 @@
-import React,{useState,useEffect} from 'react'
-import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input'
+import React, { useState, useEffect } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 import { useFormik } from "formik";
 import Select from "react-select";
-import {Country,State,City} from "country-state-city";
-import './ContactForm.css';
-import { contactSchema } from './schemas';
+import { Country, State, City } from "country-state-city";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./ContactForm.css";
+import { contactSchema } from "./schemas";
 
 function ContactForm() {
-      const [value, setValue] = useState();
-      const [records,setRecords]=useState([]);
-     
-      
-const initialValues = {
+  const [value, setValue] = useState();
+  const [records, setRecords] = useState([]);
+  const [startDate, setStartDate] = useState(new Date());
+
+  const initialValues = {
     username: "",
-    lastname:"",
+    lastname: "",
     email: "",
-    gender:"",
-    age:"",
-    country: "India",
-    state: null,
-    city: null
-    
+    gender: "",
+    age: "",
+    country: "",
+    state: "",
+    city: "",
+    pannum:"",
+    pcode:""
   };
 
-      const { values, errors, touched, handleBlur,handleChange,handleSubmit ,setFieldValue,setValues} =
-      useFormik({
-        initialValues:initialValues,
-        validationSchema: contactSchema,
-        onSubmit: (values, action) => {
-          console.log(
-            
-            values,
-            JSON.stringify(values)
-          );
-         
-          action.resetForm();
-          const headers = new Headers()
-        headers.append("Content-Type", "application/json")
+  const {
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    setFieldValue,
+    setValues,
+  } = useFormik({
+    initialValues: initialValues,
+    validationSchema: contactSchema,
 
-        const body = {
-        "userData": values
-        }
+    onSubmit: (values, action) => {
+      alert("working");
+      console.log(values, JSON.stringify(values));
+      const headers = new Headers();
+      headers.append("Content-Type", "application/json");
 
-        const options = {
+      const body = {
+        userData: values,
+      };
+
+      const options = {
         method: "POST",
         headers,
         mode: "cors",
         body: JSON.stringify(body),
-        }
+      };
 
-fetch("https://eos4ryec5bx7ucd.m.pipedream.net", options)
-        },
-      });
-  
-    
-  
-      const countries = Country.getAllCountries();
-      const updateCountries = countries.map((country)=>({
-        label:country.name,
-        value:country.isoCode,
-        ...country
+      fetch("https://eos4ryec5bx7ucd.m.pipedream.net/", options);
+    },
+  });
 
-      }));
-      const updateState=(countryCode)=>
-      State.getStatesOfCountry(countryCode).map((state)=>({
-        label:state.name,
-        value:state.isoCode,
-        ...state
-      }));
-      console.log("states",State.getStatesOfCountry
-      (93));
-      const updateCities=(countryCode,stateCode)=>
-      City.getCitiesOfState(countryCode,stateCode).map((city)=>({
-        label:city.name,
-        value:city.isoCode,
-        ...city
-      }));
- 
-      useEffect(() => {}, [values]);
-      console.log("value",values);
-      // console.log("======000===>", City.getCitiesOfState("IN", "GJ"));
+  const countries = Country.getAllCountries();
+  const updateCountries = countries.map((country) => ({
+    label: country.name,
+    value: country.isoCode,
+    ...country,
+  }));
+  const updateState = (countryCode) =>
+    State.getStatesOfCountry(countryCode).map((state) => ({
+      label: state.name,
+      value: state.isoCode,
+      ...state,
+    }));
+  console.log("states", State.getStatesOfCountry(93));
+  const updateCities = (countryCode, stateCode) =>
+    City.getCitiesOfState(countryCode, stateCode).map((city) => ({
+      label: city.name,
+      value: city.isoCode,
+      ...city,
+    }));
+
+  useEffect(() => {}, [values]);
+  console.log("value", values);
+  // console.log("======000===>", City.getCitiesOfState("IN", "GJ"));
+
   return (
     <>
-    <form className='contact-form' onSubmit={handleSubmit}>
-        <div className='form-group'>
-            <label htmlFor="name">Firstname</label>
-            <input type="text" name='username' value={values.username} onBlur={handleBlur} onChange={handleChange}  className='form-control' />     
-           { errors.username && touched.username ? <p className='form-error'>{errors.username}</p>:null }
+      <form className="contact-form" onSubmit={handleSubmit}>
+      <div className="person-detail">
+        <div className="form-group">
+        
+          
+          <input
+            type="text"
+            name="username"
+            value={values.username}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            placeholder="FirstName"
+            className="form-control "
+          />
+          {errors.username && touched.username ? (
+            <p className="form-error">{errors.username}</p>
+          ) : null}
         </div>
-        <div className='form-group'>
-        <label htmlFor="name">Lastname</label>
-            <input type="text" name='lastname' value={values.lastname} onBlur={handleBlur}  onChange={handleChange}  className='form-control' />
-            { errors.lastname && touched.lastname ? <p className='form-error'>{errors.lastname}</p>:null }
+        <div className="form-group">
+        
+          <input
+            type="text"
+            name="lastname"
+            value={values.lastname}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            placeholder="LastName"
+            className="form-control"
+          />
+          {errors.lastname && touched.lastname ? (
+            <p className="form-error">{errors.lastname}</p>
+          ) : null}
         </div>
-        <div className='form-group'>
-           <label htmlFor="emailInput">Email</label>
-           <input type="email" onChange={handleChange} onBlur={handleBlur}   value={values.email} name="email"/>
-           { errors.email && touched.email ? <p className='form-error'>{errors.email}</p>:null }
         </div>
-        <div className='form-group'>
-        <label htmlFor="">Female</label>
-            <input className='gender-e' type="radio" onChange={handleChange} name="gender" value="Female" />
-            &nbsp;
-            <label htmlFor="">Male</label>
-            <input type="radio" className='gender-m' onChange={handleChange}  name="gender" value="Male" />
-            &nbsp;
-            <label htmlFor="">Other</label>
-            <input type="radio" className='gender-o' onChange={handleChange}  name="gender" value="Other" />
+        <div className="form-group">
+         
+          <input
+            type="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+            name="email"
+            placeholder="Email"
+          />
+          {errors.email && touched.email ? (
+            <p className="form-error">{errors.email}</p>
+          ) : null}
         </div>
-        <div className='form-group'>
-          <label htmlFor="">Age</label>
-          <input type="number"  name='age' onChange={handleChange} onBlur={handleBlur} value={values.age} required/>
-          { errors.age && touched.age ? <p className='form-error'>{errors.age}</p>:null }
+        <div 
+        className="form-group">
+          <input type="text" className="pincode" placeholder="PAN-NUMBER" name="pannum"  value={values.pannum}
+            onBlur={handleBlur}
+            onChange={handleChange} />
+            {errors.pannum && touched.pannum ? (
+            <p className="form-error">{errors.pannum}</p>
+          ) : null}
         </div>
-        <div className='form-group'>
-           <label htmlFor="">Country</label>
-           <Select name="country" 
-           id="country"
-           options={updateCountries}
-           value={values.country}
-           onChange={(value)=>{
-            setValues({country:value,state:null,city:null},false);
-           }}
-           required
-           ></Select>
-           <label htmlFor="">State</label>
-           <Select name="state" 
-           id="state"
-           options={updateState(values.country ? values.country.value:null)}
-           value={values.state}
-           onChange={(value)=>{
-            setValues({state:value,city:null},false);
-           }}
-           required
-           ></Select>
-            <label htmlFor="">City</label>
-           <Select name="city" 
-           id="city"
-           options={updateCities(values.state ? values.state.countryCode : null,
-            values.state ? values.state.isoCode : null)}
-           value={values.city}
-           onChange={(value)=>{
-            setFieldValue("city", value)
-           }}
-           required
-           ></Select>
-
-            <p>{JSON.stringify(Country.get)}</p>
-            <p>{JSON.stringify(State.get)}</p>
-            <p>{JSON.stringify(City.get)}</p>
+        <div className="form-group">
+          <label htmlFor="">Female</label>
+          <input
+            className="gender-e"
+            type="radio"
+            onChange={handleChange}
+            name="gender"
+            value="Female"
+          />
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          <label htmlFor="">Male</label>
+          <input
+            type="radio"
+            className="gender-m"
+            onChange={handleChange}
+            name="gender"
+            value="Male"
+          />
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          <label htmlFor="">Other</label>
+          <input
+            type="radio"
+            className="gender-o"
+            onChange={handleChange}
+            name="gender"
+            value="Other"
+          />
         </div>
-
-        <div className='form-group'>
-        <PhoneInput
-      placeholder="Enter phone number"
-      value={value}
-      onChange={setValue}/>
+        <div className="form-group">
+        <DatePicker className="birth-d" placeholderText="birthdate" selected={startDate} onChange={(date) => setStartDate(date)} />
         </div>
-        <div className='form-group'>
-            <button>Submit</button>
+        <div className="addressline">
+            <div className="address-1">
+            
+            <textarea name="adress" placeholder="AddressLine-1"  cols="50" rows="3"></textarea>
+            </div>
+            <div className="address-2">
+        
+            <textarea name="adress" placeholder="AddressLine-2"    cols="50" rows="3"></textarea>
+            </div>
+           
+        </div>
+        <div className="form-group">
+          <input type="text" className="pincode" placeholder="pincode" name="pcode"  value={values.pcode}
+            onBlur={handleBlur}
+            onChange={handleChange} />
+          {errors.pcode && touched.pcode ? (
+            <p className="form-error">{errors.pcode}</p>
+          ) : null}
         </div>
         
+        <div className="form-group">
+          <div className="csc">
+          
+          <Select
+            name="country"
+            id="country"
+            placeholder=" Select Country"
+            options={updateCountries}
+            value={values.country}
+            onChange={(value) => {
+              setValues({ country: value, state: "", city: "" }, false);
+            }}
+            className="csc-input"
+            required
+          ></Select>
+          
+          <Select
+            name="state"
+            id="state"
+            placeholder="Select State"
+            options={updateState(values.country ? values.country.value : null)}
+            value={values.state}
+            onChange={(value) => {
+              setValues({ state: value, city: "" }, false);
+            }}
+            required
+          ></Select>
+         
+          <Select
+            name="city"
+            id="city"
+            placeholder="Select City..."
+            options={updateCities(
+              values.state ? values.state.countryCode : null,
+              values.state ? values.state.isoCode : null
+            )}
+            value={values.city}
+            onChange={(value) => {
+              setValues({ ...values, city: value }, false);
+            }}
+            required
+          ></Select>
+          </div>
 
-    </form>
-    <div>
-        {
-            records.map((curElem)=>{
-                return(
-                    <div className='showDataStyle' >
-                         <p>{curElem.username}</p>
-                         <p>{curElem.lastname}</p>
-                         <p>{curElem.email}</p>
-                    </div>
-                )
-            })
-        }
-    </div>
+          <p>{JSON.stringify(Country.get)}</p>
+          <p>{JSON.stringify(State.get)}</p>
+          <p>{JSON.stringify(City.get)}</p>
+        </div>
+
+        <div className="form-group">
+          <div className="phone-input">
+          <PhoneInput
+            placeholder="Enter phone number"
+            value={value}
+            onChange={setValue}
+            required
+           
+          />
+          </div>
+        </div>
+        <div className="form-group">
+          <button type="button">Submit</button>
+        </div>
+      </form>
+      <div>
+        {records.map((curElem) => {
+          return (
+            <div className="showDataStyle">
+              <p>{curElem.username}</p>
+              <p>{curElem.lastname}</p>
+              <p>{curElem.email}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -1,8 +1,12 @@
 import * as Yup from "yup";
 
 export const contactSchema=Yup.object({
-    username:Yup.string().min(3).max(20).required("please enter your firstname"),
-    lastname:Yup.string().min(3).max(20).required("please enter your lastname"),
+    username:Yup.string().min(3).max(20).matches(/^[a-zA-Z\-]+$/,"please not enter witespace").required("please enter your firstname"),
+    lastname:Yup.string().min(3).max(20).matches(/^[a-zA-Z\-]+$/,"please not enter witespace").required("please enter your lastname"),
     email:Yup.string().email().required("email is required"),
-    age:Yup.number().required().positive().integer()
+    age:Yup.number().required().positive().integer(),
+    gender:Yup.string().min(1).required("please select gender"),
+    pannum:Yup.string().matches(/[A-Z]{5}[0-9]{4}[A-Z]{1}/,"pan number must in correct format"),
+    pcode:Yup.string().matches(/^[1-9][0-9]{5}$/,"please enter valid pincode")
+
 })
